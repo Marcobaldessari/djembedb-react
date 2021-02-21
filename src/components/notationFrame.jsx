ac051f589242198b94de944e78645d63520ea221
Marco Baldessari <marco.baldes@gmail.com>
2021-02-21 20:36:50 +0100

    note logic added to notation frame

--- src/components/notationFrame.jsx
+++ src/components/notationFrame.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import NotationNote from "./notationNote";
 
 class NotationFrame extends Component {
   state = {
     song: this.props.song,
+    notes: "",
   };
 
+  //   var notes;
   render() {
-    return <div className="notation-frame"></div>;
+    return (
+      <div className="notation-frame">
+        <p>{this.getNote()}</p>
+        {this.state.notes.map((i) => (
+          <NotationNote key={i} note={i} />
+        ))}
+      </div>
+    );
   }
+
+  getNote = () => {
+    this.state.notes = this.props.song.match(/.{1,2}/g);
+  };
 }
 
 export default NotationFrame;
