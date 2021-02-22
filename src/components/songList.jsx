05e1b83c6928ec9e3bf4c8bf6fb235b969e7993e
Marco Baldessari <marco.baldes@gmail.com>
2021-02-22 11:44:17 +0100

    refactored songs data

--- src/components/songList.jsx
+++ src/components/songList.jsx
@@ -1,66 +1,14 @@
 import React, { Component } from "react";
 import SongCard from "./songCard";
+import { SongsData } from "../songsData";
 
 class SongList extends Component {
-  state = {
-    songs: [
-      {
-        id: 1,
-        songName: "Bala Kulandyan",
-        song: "Pa x x Ta Pa x go do Pa x x Ta Pa x go do",
-      },
-      {
-        id: 2,
-        songName: "Modern hip pop",
-        song: "Gun do go do Pa x x do Gun x Gun x do Pa x x",
-      },
-      {
-        id: 3,
-        songName: "Bala Kulandyan",
-        song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-      },
-      {
-        id: 4,
-        songName: "Balané",
-        song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-      },
-      {
-        id: 5,
-        songName: "Bandon Djeli",
-        song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-      },
-      {
-        id: 6,
-        songName: "Bala Kulandyan",
-        song: "Pa x x Ta Pa x go do Pa x x Ta Pa x go do",
-      },
-      {
-        id: 7,
-        songName: "Modern hip pop",
-        song: "Gun do go do Pa x x do Gun x Gun x do Pa x x",
-      },
-      {
-        id: 8,
-        songName: "Bala Kulandyan",
-        song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-      },
-      {
-        id: 9,
-        songName: "Balané",
-        song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-      },
-      {
-        id: 10,
-        songName: "Bandon Djeli",
-        song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-      },
-    ],
-  };
+  state = {};
 
   render() {
     return (
       <div>
-        {this.state.songs.map((song) => (
+        {SongsData.map((song) => (
           <SongCard key={song.id} songName={song.songName} song={song.song} />
         ))}
       </div>
---
+++ src/songsData.js
@@ -0,0 +1,52 @@
+export const SongsData = [
+    {
+      id: 1,
+      songName: "Bala Kulandyan",
+      song: "Pa x x Ta Pa x go do Pa x x Ta Pa x go do",
+    },
+    {
+      id: 2,
+      songName: "Modern hip pop",
+      song: "Gun do go do Pa x x do Gun x Gun x do Pa x x",
+    },
+    {
+      id: 3,
+      songName: "Bala Kulandyan",
+      song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
+    },
+    {
+      id: 4,
+      songName: "Balané",
+      song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
+    },
+    {
+      id: 5,
+      songName: "Bandon Djeli",
+      song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
+    },
+    {
+      id: 6,
+      songName: "Bala Kulandyan",
+      song: "Pa x x Ta Pa x go do Pa x x Ta Pa x go do",
+    },
+    {
+      id: 7,
+      songName: "Modern hip pop",
+      song: "Gun do go do Pa x x do Gun x Gun x do Pa x x",
+    },
+    {
+      id: 8,
+      songName: "Bala Kulandyan",
+      song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
+    },
+    {
+      id: 9,
+      songName: "Balané",
+      song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
+    },
+    {
+      id: 10,
+      songName: "Bandon Djeli",
+      song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
+    },
+  ]
