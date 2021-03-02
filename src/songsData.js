47e86dd4152c571b82825a3370ab15960aa36554
Marco Baldessari <marco.baldes@gmail.com>
2021-03-02 14:40:54 +0100

    fixes

--- src/songsData.js
+++ src/songsData.js
@@ -43,110 +43,68 @@ export const SongsData = [
       ]
       ,
     },
+
+    {
+      songName: "Djansa",
+      song: [
+        "Pa Ta xx Ta Pa xx do go",
+        "Ta xx Gun xx Ta Dun go do"
+        
+      ]
+      ,
+    },
+    {
+      songName: "Kono",
+      src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
+      song: [
+        "Gun do go do Gun xx Ta Pa",
+        "Gun do go do Gun xx Pa xx"
+      ]
+      ,
+    },
+    {
+      songName: "Yankadi",
+      src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
+      song: [
+        "Gun xx go do Gun Ta Pa xx",
+        "Gun xx go xx Gun Ta Pa xx"
+      ]
+      ,
+    },
+    {
+      songName: "Kuku",
+      src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
+      song: [
+        "Gun Ca xx Gun Dun go do Gun",
+        "Dun Ca xx Gun Dun go do Gun"
+      ]
+      ,
+    },
     // {
-    //   songName: "Modern beat",
-    //   song: [
-    //     "xx Gun xx Pa xx Gun xx xx",
-    //     "xx Gun xx do Pa xx xx xx"
-    //   ]
-      
-    // },
-    {
-      songName: "Djansa",
-      song: [
-        "Pa Ta xx Ta Pa xx do go",
-        "Ta xx Gun xx Ta Dun go do"
-        
-      ]
-      ,
-    },
-    {
-      songName: "Djansa",
-      song: [
-        "Pa Ta xx Ta Pa xx do go",
-        "Ta xx Gun xx Ta Dun go do"
-        
-      ]
-      ,
-    },
-    {
-      songName: "Djansa",
-      song: [
-        "Pa Ta xx Ta Pa xx do go",
-        "Ta xx Gun xx Ta Dun go do"
-        
-      ]
-      ,
-    },
-    {
-      songName: "Djansa",
-      song: [
-        "Pa Ta xx Ta Pa xx do go",
-        "Ta xx Gun xx Ta Dun go do"
-        
-      ]
-      ,
-    },
-    {
-      songName: "Djansa",
-      song: [
-        "Pa Ta xx Ta Pa xx do go",
-        "Ta xx Gun xx Ta Dun go do"
-        
-      ]
-      ,
-    },
-    
-    
-    
-    // {
-    //   songName: "Patatje",
+    //   songName: "Didadi",
     //   src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
     //   song: [
-    //     "Gun xx Ta Pa xx Dun Dun Ta",
-    //     "xx Gun xx Ta Pa xx Dun Dun Ta Pa xx"
+    //     "Gun do Gun do Gun Gun xx go do Pa Ta xx",
+    //     ""
     //   ]
     //   ,
     // },
-    
-    // {
-    //   songName: "Test",
-    //   song: [
-    //     "Gun Gun Gun Gun Ca Ca Ca Ca",
-    //     "go go go go Ta Ta Ta Ta"
-    //   ]
-    //   ,
-    // },
-    // {
-    //   songName: "Bala Kulandyan",
-    //   song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-    // },
-    // {
-    //   songName: "Balané",
-    //   song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-    // },
-    // {
-    //   songName: "Bandon Djeli",
-    //   song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-    // },
-    // {
-    //   songName: "Bala Kulandyan",
-    //   song: "Pa xx xx Ta Pa xx go do Pa xx xx Ta Pa xx go do",
-    // },
-    // {
-    //   songName: "Modern hip pop",
-    //   song: "Gun do go do Pa xx xx do Gun xx Gun xx do Pa xx xx",
-    // },
-    // {
-    //   songName: "Bala Kulandyan",
-    //   song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-    // },
-    // {
-    //   songName: "Balané",
-    //   song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-    // },
-    // {
-    //   songName: "Bandon Djeli",
-    //   song: "Dun Gun do go Ta Pa Dun Gun do go Ta Pa Dun Gun do go",
-    // },
+      {
+        songName: "Bala Kulandyan",
+        song: [
+          "Pa xx xx Ta Pa xx go do",
+          "Pa xx xx Ta Pa xx go do"
+        ]
+        ,
+      },
+  
+      {
+        songName: "Djansa",
+        song: [
+          "Pa Ta xx Ta Pa xx do go",
+          "Ta xx Gun xx Ta Dun go do"
+          
+        ]
+        ,
+      },
   ]
