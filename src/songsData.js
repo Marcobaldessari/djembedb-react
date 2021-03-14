ff1522e210fbdfab9418d5b9861581908f1bff56
Marco Baldessari <marco.baldes@gmail.com>
2021-03-14 22:42:37 +0100

    removes duplicates

--- src/songsData.js
+++ src/songsData.js
@@ -164,63 +164,11 @@ export const SongsData = [
       ]
       ,
     },
-    {
-      songName: "Kono",
-      src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
-      timeSignature: "4/4",
-      song: [
-        "Gun do go do Gun xx Ta Pa",
-        "Gun do go do Gun xx Pa xx"
-      ]
-      ,
-    },
-    {
-      songName: "Yankadi",
-      src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
-      timeSignature: "4/4",
-      song: [
-        "Gun xx go do Gun Ta Pa xx",
-        "Gun xx go xx Gun Ta Pa xx"
-      ]
-      ,
-    },
-    {
-      songName: "Kuku",
-      src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
-      timeSignature: "4/4",
-      song: [
-        "Gun Ca xx Gun Dun go do Gun",
-        "Dun Ca xx Gun Dun go do Gun"
-      ]
-      ,
-    },
-    // {
-    //   songName: "Didadi",
-    //   src: "https://www.youtube.com/watch?v=5HIAuK9RS6E&t=8s",
-    //   song: [
-    //     "Gun do Gun do Gun Gun xx go do Pa Ta xx",
-    //     ""
-    //   ]
-    //   ,
-    // },
-      {
-        songName: "Bala Kulandyan",
-        timeSignature: "4/4",
-        song: [
-          "Pa xx xx Ta Pa xx go do",
-          "Pa xx xx Ta Pa xx go do"
-        ]
-        ,
-      },
+
+
+
+
+
   
-      {
-        songName: "Djansa",
-        timeSignature: "4/4",
-        song: [
-          "Pa Ta xx Ta Pa xx do go",
-          "Ta xx Gun xx Ta Dun go do"
-          
-        ]
-        ,
-      },
+
   ]
